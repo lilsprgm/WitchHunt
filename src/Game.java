0c9974cdb9d0a378d15a3884d211c5c7f23754ae
Lilian Bregnon <lils.bregnon@gmail.com>
2021-11-29 13:58:34 +0100

    Debut de la javadoc

--- src/Game.java
+++ src/Game.java
@@ -91,6 +91,16 @@ public class Game extends Observable {
         this.cards = value;
     }
 */
+
+    /**
+     * intiStockPile est la fonction qui permet d'instancier les cartes action du jeu.
+     * Les cartes sont crées puis stockées dans la collection de la classe game appelé "Stockpile".
+     * Après la création du tas de cartes on les mélange ("suffle(stockPile);") pour pouvoir les distribuer directement ensuite.
+     * La distribution ne s'effectue pas dans cette méthode.
+     *
+     * @author lilsb
+     *
+     */
     public void initStockPile(){ //Initialisation de la pioche
         stockPile.add(new AngryMob());
         stockPile.add(new BlackCat());
@@ -107,12 +117,26 @@ public class Game extends Observable {
         suffle(stockPile);
     }
 
+    /**
+     * Cette fonction permet de piocher aléatoirement une carte dans un tas
+     * @param cards est la collection du tas de carte dans lequel on souhaite piocher.
+     * @return on retourne la carte qui a été piochée.
+     *
+     * @author lilsb
+     */
     public Card draw(@NotNull List<Card> cards){ // pour piocher
         Card card = cards.get(0);// on prend toujours l'index 1 car dans tout les cas les cartes sont mélangés
         cards.remove(0);
         return card;
 
     }
+
+    /**
+     * Permet de melanger n'importe quelle collection de cartes.
+     * @param cards correspond au tas de cartes que l'on souhaite mélanger.
+     *
+     * @author lilsb
+     */
     public void suffle(List<Card> cards){   // Pour être plus clair et rapide si on a besoin de melanger n'importe quoi par ex la main d'un joueur
         Collections.shuffle(cards);
     }
@@ -155,6 +179,219 @@ public class Game extends Observable {
             System.out.println(player_temp.getName());
         }
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    }
+
+    /**
+     * Fonction permettant de regrouper les fonctions pour ddébuter une partie.
+     *
+     * @author lilsb
+     */
+    public void init_Game(){
+        this.initPlayers();
+        this.initStockPile();
+    }
+
+    /**
+     * Permet de débuter un nouveau round. Pour se faire, on supprime toutes les cartes des joueurs et on leur redistribue de noouvelles cartes.
+     * Le nombre de cartes distribuées par personne est déterminé automatiquement pour que tous les joueurs aient le même nombre de cartes.
+     *
+     * @author lilsb
+     */
+    public void initNewRound (){
+        stockPile.removeAll(stockPile);
+        initStockPile();
+        for (Player player : players){
+            player.clearDeck();
+        }
+        while (stockPile.size() - (numberOfPlayer + numberOfBot) >= 0){
+            for (Player player : players){
+                player.addCardToDeck(draw(stockPile));
+            }
+        }
+        for (Player player : players){
+            System.out.println(player.getName() + " -->");
+            player.chooseIdentity();
+        }
+    }
+
+    /**
+     * Permet de déterminer si le round est terminé (rappel round terminé si l'identité de tous les joueurs a été révélée sauf un).
+     * Cette fonction gère aussi l'attributiion des points à la fin du round, pour le joueur qui n'a pas été découvert.
+     * @return true si c'est la fin du round, false si ca ne l'est pas.
+     *
+     * @author lilsb
+     */
+    public boolean endOfRound(){ // fonction pour déterminer si le round est termiiné ou pas
+        int numberOfRevealed = 0;
+        for (Player player : players){
+            if (player.getIdentity().isRevealed()) {
+                numberOfRevealed++;
+            }
+        }
+        if (numberOfRevealed == players.size() - 1){
+            System.out.println("End of the round !!");
+
+            // partie pour compter les points si la personne n'est pas decouverte
+            for (Player player :players){
+                if (!player.getIdentity().isRevealed()){
+                    if (player.getIdentity().getRole() == Role.Witch){
+                        player.addPoints(2);
+                    }
+                    else{
+                        player.addPoints(1);
+                    }
+                }
+            }
+            //
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * Permet de récupérer le joueurs suivant à joueur, qui n'est pas celui jouant naturellement apres dans l'ordre de jeu.
+     * Ce joueur est stocké dans une variable et on retourne ce joueur.
+     * @return le joueur qui doit joueur ensuite.
+     *
+     * @author lilsb
+     */
+    public Player getChosenNextPLayer (){
+        return chosenNextPlayer;
+    }
+
+    /**
+     * Permet de stocker le nom du joueur suivant désigné.
+     * @param player le joueur qui a été désigné pour jouer en suivant
+     *
+     * @author lilsb
+     */
+    public void chooseNextPlayer(Player player){
+        chosenNextPlayer = player;
+    }
+
+    /**
+     * Permet de choisir le nom du joueur suivant.
+     * On affiche le nom de tous les joueurs pouvant être choisi puis l'utilisateur rentre le nom du joueur qu'il veut choisir.
+     * @param exception le joueur qui ne pourra pas être désigné comme étant le joueur suivant.
+     * (Parce que c'est lui qui appelle la fonction par exemeple donc il ne pourra pas se désigner lui même).
+     * @return le joueur choisi.
+     *
+     * @author lilsb
+     */
+    public Player chooseAPlayer(Player exception){
+        for (Player player : players){
+            if (player == exception){
+                continue;
+            }
+            System.out.println(player);
+        }
+        String nameOfChosenPlayer = s.nextLine(); // attention a revoir car exception si on se trompe lorsque l'on tape le nom.
+        // regarder avec la fonction scanner .next(pattern))
+        for (Player player : players){
+            if (player.getName() == nameOfChosenPlayer){
+                return player;
+            }
+        }
+    }
+
+    /**
+     * Fonction quui permet de gérer la phase de jeu correspondant à une accusation.
+     * Elle regroupe le choix du joueur accusé, l'action du joueur accusé, ainsi que l'attribution des points à la fin de l'accusation.
+     * @param accusator le joueur qui accuse. Ce paramettre permet d'attribuer les points à celui-ci à la fin du tour.
+     *
+     * @author lilsb
+     */
+    public void accusation(Player accusator){ // on transmet en parametre d'entré le joueur qui accuse
+        System.out.println("Who do you want to accuse ?");
+        Player accusedPlayer = chooseAPlayer(accusator);
+        accusedPlayer.setAccused(true);
+        accusedPlayer.play();
+        accusedPlayer.setAccused(false);
+        if (accusedPlayer.getIdentity().isRevealed() & accusedPlayer.getIdentity().getRole() == Role.Witch){
+            accusator.addPoints(1);
+            System.out.println(accusator.getName() + " won 1 point");
+        }
+
+    }
+
+    /**
+     * Permet la gestion d'un round complet. Tant que le round n'est pas fini cette fonction permet de gérer l'ordre de jeu ses joueurs.
+     * Il y a deux cas possible : soit on suit l'orde "naturel" de jeu des joueurs,
+     * soit le joueurs suivant a été désigné par l'action d'une carte d'un joueurs.
+     * Quand on arrive au dernier joueurs à jouer, on refait jouer le premier.
+     *
+     * @author lilsb
+     */
+    public void roundManagement(){ // pour gerer à qui cest le tour de jouer jsp sii cest tres otpi ?
+        int index = 0;
+        Player currentPLayer = players.get(index);
+        while (!endOfRound()){
+            System.out.println("Your turn "+ currentPLayer.getName());
+           currentPLayer.play();
+            if (chosenNextPlayer != null){
+               currentPLayer = chosenNextPlayer;
+               chooseNextPlayer(null);
+               index = players.indexOf(currentPLayer);
+               continue;
+            }
+            else if (currentPLayer == players.getLast()){
+                currentPLayer = players.getFirst();
+                index = 0;
+                continue;
+            }
+            else {
+                index++;
+                currentPLayer = players.get(index);
+            }
+        }
+    }
+
+    /**
+     * Permet de déterminer si la partie est finie ou non. (Rappel fin de partie si tous les joueurs ont auu moins 5 points).
+     * @return true si c'est la fin de la partie sinon false.
+     *
+     * @author lilsb
+     */
+    public boolean endOfGame(){
+        for (Player player : players){
+            if (player.getNumberOfPoints() < 5){
+                return false;
+            }
+        }
+        System.out.println("End of the game");
+        return true;
+    }
+
+    /**
+     * Permet d'afficher le nom du ou des gagnants de la partie.
+     * On regarde quel est/ sont les joueurs ayant le plus de points.
+     *
+     * @author lilsb
+     */
+    public void theWinnerIs(){ // fonction permettant d'afficher les gagnants
+        List<Player> winner = new ArrayList<Player>();
+        winner.add(players.getFirst());
+        for (Player player : players){
+            if (player.getNumberOfPoints() > winner.get(0).getNumberOfPoints()){
+                winner.removeAll(winner);
+                winner.add(player);
+            }
+            else if (player.getNumberOfPoints() == winner.get(0).getNumberOfPoints()){
+                winner.add(player);
+            }
+        }
+        System.out.println("----- WINNER -----");
+        for (Player player : winner){
+            System.out.println(player);
+        }
+
+    }
+    public void round(){
+        do{
+
+        }while(true);
     }
 
     public void game(){
