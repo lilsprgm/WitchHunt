e2523c5de60fcd36221c6c3474d0b8736e4f21a3
Myxe <nizaragougile@gmail.com>
2021-11-30 19:47:58 +0100

    Code de la carte Wart + Commentaires Javadoc

--- src/Cards/Wart.java
+++ src/Cards/Wart.java
@@ -1,28 +1,44 @@
 package Cards;
 
+import Game_operator.Player;
+
 public class Wart extends Card {
 
     public Wart(){
         setName("Wart");
+        setActionHunt("Choose next player");
+        setActionWitch("Take next turn");
+    }
+
+    /**
+     * Permet d'activer l'effet Witch de la carte Wart.
+     *Le Joueur est le prochain à jouer.
+     * @param player Représente le joueur qui a jouer la carte.
+     */
+    @Override
+    public void actionWitch(Player player) {
+        System.out.println(getActionWitch());
+        player.getGame().chooseNextPlayer(player);
+    }
+
+    /**
+     * Permet d'activer l'effet Hunt de la carte Wart.
+     *Le Joueur doit choisir parmis les Joueurs de la partie le prochain à jouer.
+     * @param player Représente le joueur qui a jouer la carte.
+     */
+    @Override
+    public void actionHunt(Player player) {
+        System.out.println(getActionHunt());
+        player.getGame().chooseNextPlayer(player.getGame().chooseAPlayer(player));
     }
 
     @Override
-    public void actionWitch() {
-
+    public boolean conditionWitch(Player player) {
+        return true;
     }
 
     @Override
-    public void actionHunt() {
-
-    }
-
-    @Override
-    public boolean conditionWitch() {
-
-    }
-
-    @Override
-    public boolean conditionHunt() {
-
+    public boolean conditionHunt(Player player) {
+        return true;
     }
 }
