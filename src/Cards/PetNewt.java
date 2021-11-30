eef04c2405df2c29735918b6bca669b0297d7edb
Myxe <nizaragougile@gmail.com>
2021-11-30 19:42:13 +0100

    Ajout des commentaires Javadoc

--- src/Cards/PetNewt.java
+++ src/Cards/PetNewt.java
@@ -13,17 +13,20 @@ public class PetNewt extends Card {
 
     @Override
     public void actionWitch(Player player) {
+        System.out.println(getActionWitch());
         player.getGame().chooseNextPlayer(player);
     }
 
     /**
      * Permet d'activer l'effet Hunt de la carte PetNewt.
-     * Le Joueur va devoir choisir parmis la liste de tous les joueurs de la partie celui à qui il va prendre
+     * Le Joueur va devoir choisir parmis la liste de tous les joueurs de la partie celui à qui il souhaite prendre
      * une carte Rumeur déja révélée. La liste des Joueurs apparaitra avec leur
-     * nombre de carte Rmeur révélée, puis la liste de ces cartes révélées.
+     * nombre de carte Rumeur révélée, puis la liste de ces cartes révélées ensuite.
+     * @param player Représente le joueur qui a jouer la carte.
      */
     @Override
     public void actionHunt(Player player) {
+        System.out.println(getActionHunt());
         int i=1;
         System.out.println("Choose a player from whom you want to take a revealed Card : \n");
         for(Player p : player.getGame().getPlayers()){
@@ -45,6 +48,8 @@ public class PetNewt extends Card {
     /**
      * Permet de vérifier si le Joueur peut jouer la carte PetNewt ou non.
      * On vérifie pour tout les Joueurs de la partie si au moins 1 dispose de carte Rumeur révélée.
+     * @param player Représente le joueur qui a jouer la carte.
+     * @return la réponse à la condition.
      */
     @Override
     public boolean conditionHunt(Player player) {
